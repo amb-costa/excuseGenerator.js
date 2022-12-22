af25c31449eebbf92722be26467c1e706f7906ba
beinganidiot <ocaranzacosta@gmail.com>
2022-12-22 15:40:48 +0000

    final work

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
+import "./index.html";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onLoad = function() {
+  let par = document.querySelector("#excuse");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let sent = "";
+  sent += who[rndm(who.length)] + " ";
+  sent += action[rndm(action.length)] + " ";
+  sent += what[rndm(what.length)] + " ";
+  sent += when[rndm(when.length)];
+  par.innerHTML = sent;
 };
+
+function rndm(num) {
+  return Math.floor(Math.random() * num);
+}
